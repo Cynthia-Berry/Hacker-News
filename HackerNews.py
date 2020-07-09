bc2f02c1f9a81848529907bf723cfb8dc43f2aba
Cynthia Nwakaeme <nwakaemecynthia@gmail.com>
2020-07-09 01:59:43 +0100

    update Haker News file

--- HackerNews.py
+++ HackerNews.py
@@ -3,14 +3,18 @@
 
 # # PROJECT ON HACKER NEWS
 
-# `Hacker News` is a site started by the startup incubator Y Combinator, where user-submitted stories (known as "posts") are voted and commented upon, similar to reddit. Hacker News is extremely popular in technology and startup circles, and posts that make it to the top of Hacker News' listings can get hundreds of thousands of visitors as a result.
-# 
-# We're specifically interested in posts whose titles begin with either `Ask HN` or `Show HN`. Users submit Ask HN posts to ask the Hacker News community a specific question.
+# `Hacker News` is a site started by the startup incubator Y Combinator,
+# where user-submitted stories (known as "posts") are voted and commented upon, similar to reddit.
+#
+# Hacker News is extremely popular in technology and startup circles, and posts that make it to the top of Hacker News'
+# listings can get hundreds of thousands of visitors as a result.
+# We're specifically interested in posts whose titles begin with either `Ask HN` or `Show HN`.
+# Users submit Ask HN posts to ask the Hacker News community a specific question.
 # We'll compare these two types of posts to determine the following:
 # 
 # Do `Ask HN` or `Show HN` receive more comments on average?
 # Do posts created at a certain time receive more comments on average?
-# 
+
 
 # In[1]:
 
@@ -226,7 +230,7 @@ for element in sorted_swap[:5]:
 # * 21:00 (9PM)
 # 
 
-# In[ ]:
+
 
 
 
